6fe036e3d5ccb2cc8a4fc1559d669c3bcc063650
sirius <i@sirius.cool>
2023-10-12 14:39:36 +0800

    fix download script

--- frp/download-release.sh
+++ frp/download-release.sh
@@ -7,10 +7,9 @@ partial_file_name="linux_amd64"
 latest_release=$(curl -s "https://api.github.com/repos/${repo_owner}/${repo_name}/releases/latest")
 download_url=$(echo "${latest_release}" | grep "browser_download_url" | awk -F '"' '{print $4}')
 
-file_list=$(curl -s "${download_url}" | grep -oP "(?<=\"name\": \")[^\"]+")
 
 matched_files=()
-for file in ${file_list}; do
+for file in ${download_url}; do
   if [[ "${file}" == *"${partial_file_name}"* ]]; then
     matched_files+=("${file}")
   fi
@@ -18,8 +17,8 @@ done
 
 if [ ${#matched_files[@]} -gt 0 ]; then
   file_to_download=${matched_files[0]}
-  file_url="${download_url}/${file_to_download}"
-  curl -sLO "${file_url}" -o frp.tar.gz
+  file_url="${file_to_download}"
+  wget "${file_url}" -O frp.tar.gz
   echo "Downloaded: ${file_to_download}"
   tar -xf frp.tar.gz -C ./files
   cp ./files/frps ./frps
